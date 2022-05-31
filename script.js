bdaee14347998dfe58fe8f8ed052a5e30d7fd2cc
João Marcos <jottasintonia@gmail.com>
2022-05-30 23:29:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+      x: 8 * box,
+      y: 8 * box,
+}
 
 function criarBG() {
       context.fillStyle = "lightgreen";
